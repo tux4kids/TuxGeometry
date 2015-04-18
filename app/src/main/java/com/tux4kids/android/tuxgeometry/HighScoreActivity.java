843a6c5c8d570e92b900d4ed9850b1b33e4a96da
BakonJarser <jman88888@yahoo.com>
2015-04-18 12:24:10 -0700

    Added an image of Tux to the welcome screen and high score screens. Fixed bug where if no high score existed it would display "Large Text"

--- app/src/main/java/com/tux4kids/android/tuxgeometry/HighScoreActivity.java
+++ app/src/main/java/com/tux4kids/android/tuxgeometry/HighScoreActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Jason Barker on 3/16/2015.
@@ -21,6 +22,7 @@ public class HighScoreActivity extends Activity {
         setContentView(R.layout.high_score_choice);
 
         Button areaGame = (Button) findViewById(R.id.areaHighScoreButton);
+        ImageButton imageAreaGame = (ImageButton) findViewById(R.id.areaImageButton);
 
         areaGame.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,9 +31,24 @@ public class HighScoreActivity extends Activity {
             }
         });
 
+        imageAreaGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAreaScores(AreaHighScores.class);
+            }
+        });
+
         Button matchingGame = (Button) findViewById(R.id.mathingHighScoreButton);
+        ImageButton imageMatchingGame = (ImageButton) findViewById(R.id.matchingImageButton);
 
         matchingGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAreaScores(MatchingHighScores.class);
+            }
+        });
+
+        imageMatchingGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showAreaScores(MatchingHighScores.class);
